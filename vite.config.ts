448f1387f4654034b34c6bffbd1a6f2700139822
gyalxen69 <blak.curry@gmail.com>
2026-01-06 00:51:45 +0100

    correction done for publishing the page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ktm-tourism/",
   server: {
     host: "::",
     port: 8080,
