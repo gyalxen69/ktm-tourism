af3d8e413206a39ae0feb36f161d42bde57f1bf6
gyalxen69 <blak.curry@gmail.com>
2026-01-06 12:41:25 +0100

    changes for the video

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -47,7 +47,7 @@ function VideoSection() {
             preload="metadata"
             poster=""
           >
-            <source src="/media/videos/kathmandu-tour.mp4" type="video/mp4" />
+            <source src={process.env.PUBLIC_URL + "/media/videos/kathmandu-tour.mp4"} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
